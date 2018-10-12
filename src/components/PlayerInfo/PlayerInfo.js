134152af32fe604eced8ed99cde93426ae80eaf4
ankaziele <ankaziele@gmail.com>
2018-10-12 17:04:51 +0200

    add styles with RWD

--- src/components/PlayerInfo/PlayerInfo.js
+++ src/components/PlayerInfo/PlayerInfo.js
@@ -1,27 +1,37 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import './PlayerInfo.css'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "./PlayerInfo.css";
 
 class PlayerInfo extends Component {
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired
+  };
 
-    static propTypes = {
-        name: PropTypes.string.isRequired,
-        image: PropTypes.string.isRequired
-    }
-
-    render() {
-        return (
-            <header className="playerHeader">
-                <div className="player-container">
-                    <img className="player-image" src={this.props.image} alt="User Avatar" />
-                    <div>
-                        <h2>Player:</h2>
-                        <h2 className="nick">{this.props.name}</h2>
-                    </div>
-                </div>
-            </header>
-        );
-    }
+  render() {
+    return (
+      <header className="playerHeader">
+        <h1>Player Info</h1>
+        <div className="player-container">
+          <img
+            className="player-image"
+            src={this.props.image}
+            alt="User Avatar"
+          />
+          <div className="player-info">
+            <div>
+              <h2>Player:</h2>
+              <h2 className="nick">{this.props.name}</h2>
+            </div>
+            <div>
+              <h2>Ranking:</h2>
+              <h2>Points:</h2>
+            </div>
+          </div>
+        </div>
+      </header>
+    );
+  }
 }
 
 export default PlayerInfo;
