0724d7e12c7cca75c1477f7d3ca4d441ce5fe1f4
Tomek_M <tomasz.milaszewski@gmail.com>
2018-10-13 17:36:11 +0200

    104 adds condition for finished game

--- src/components/TournamentPair/TournamentPair.js
+++ src/components/TournamentPair/TournamentPair.js
@@ -4,23 +4,25 @@ import './TournamentPair.css';
 
 class TournamentPair extends Component {
 
-static propTypes = {
-    gameId: PropTypes.number,
-    playerOne: PropTypes.string,
-    playerTwo: PropTypes.string,
-    scoreOne: PropTypes.number,
-    scoreTwo: PropTypes.number,
-}
+    static propTypes = {
+        gameId: PropTypes.number,
+        playerOne: PropTypes.string,
+        playerTwo: PropTypes.string,
+        scoreOne: PropTypes.number,
+        scoreTwo: PropTypes.number,
+    }
 
-render() {
-    return (
-        <div className="TournamentPair-container">
-            <div className="TournamentPair-player-box"><span>Ben</span></div>
-            <div className="TournamentPair-score-box"><span>3</span></div>
-            <div className="TournamentPair-score-box"><span>4</span></div>
-            <div className="TournamentPair-player-box"><span>Johnatan</span></div>
-        </div>
-    )
+    render() {
+        if (this.props.scoreOne && this.props.scoreTwo) {
+            return (
+                <div className="TournamentPair-container">
+                    <div className="TournamentPair-player-box"><span>{this.props.playerOne}</span></div>
+                    <div className="TournamentPair-score-box"><span>{this.props.scoreOne}</span></div>
+                    <div className="TournamentPair-score-box"><span>{this.props.scoreTwo}</span></div>
+                    <div className="TournamentPair-player-box"><span>{this.props.playerTwo}</span></div>
+                </div>
+            )
+        }
     }
 }
 
--- src/components/TournamentPair/TournamentPair.md
+++ src/components/TournamentPair/TournamentPair.md
@@ -10,8 +10,12 @@ TournamentPair - two players assigned - before game - no score:
 ```js
 <TournamentPair/>
 ```
-TournamentPair - two players assigned - after game - 1 player won:
+TournamentPair - two players assigned - after game - player no1 won:
 ```js
-<TournamentPair/>
+<TournamentPair playerOne={'Ben'} playerTwo={'John'} scoreOne={2} scoreTwo={1}/>
+```
+TournamentPair - two players assigned - after game - player no2 won:
+```js
+<TournamentPair playerOne={'Ben'} playerTwo={'John'} scoreOne={1} scoreTwo={2}/>
 ```
 
