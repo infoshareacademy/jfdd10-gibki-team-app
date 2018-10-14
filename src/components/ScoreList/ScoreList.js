a7c25effe3ba14df7847e7824bf4496c485afb90
Tomek_M <tomasz.milaszewski@gmail.com>
2018-10-14 13:51:10 +0200

    105 adds layout for the ScoreList component

---
+++ src/components/ScoreList/ScoreList.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import TournamentPair from '../TournamentPair/TournamentPair'
+import './ScoreList.css';
+
+class ScoreList extends Component {
+
+    static propTypes = {
+        tournamentId: PropTypes.number.isRequired
+    }
+
+    render() {
+        return (
+            <div className="ScoreList-container">
+                <div>
+                    <h2>Round 1</h2>
+                    <TournamentPair playerOne={'Ben'} playerTwo={'John'} scoreOne={1} scoreTwo={2}/>
+                    <TournamentPair playerOne={'Ben'} playerTwo={'John'} scoreOne={1} scoreTwo={2}/>
+                    <TournamentPair playerOne={'Ben'} playerTwo={'John'} scoreOne={1} scoreTwo={2}/>
+                    <TournamentPair playerOne={'Ben'} playerTwo={'John'} scoreOne={1} scoreTwo={2}/>
+                </div>
+                <div>
+                    <h2>Semi-finals</h2>
+                    <TournamentPair playerOne={'Ben'} playerTwo={'John'} scoreOne={1} scoreTwo={2}/>
+                    <TournamentPair playerOne={'Ben'} playerTwo={'John'} scoreOne={1} scoreTwo={2}/>
+                </div>
+                <div>
+                    <h2>Final</h2>
+                    <TournamentPair playerOne={'Ben'} playerTwo={'John'} scoreOne={1} scoreTwo={2}/>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ScoreList
