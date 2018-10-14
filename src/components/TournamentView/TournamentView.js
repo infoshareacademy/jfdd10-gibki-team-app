cc422363974547104b6ad10a7d552f01dccfcd8b
Tomek_M <tomasz.milaszewski@gmail.com>
2018-10-14 21:05:09 +0200

    95&104 WIP - props

--- src/components/TournamentView/TournamentView.js
+++ src/components/TournamentView/TournamentView.js
@@ -9,6 +9,9 @@ class TournamentView extends Component {
 
     state = {
         tournaments: [],
+        // tournament: [],
+        // games: [],
+
         tournamentStatus: 'finished', //temporary value - will be taken from tournaments
         players: []
     }
@@ -21,21 +24,28 @@ class TournamentView extends Component {
         fetch(process.env.PUBLIC_URL + "/data/tournaments.json")
             .then(response => response.json())
             .then(arrayOfTournaments => this.setState({ tournaments: arrayOfTournaments }));
-
-        fetch(process.env.PUBLIC_URL + "/data/players.json")
+            console.log(this.state.tournaments)
+        
+            fetch(process.env.PUBLIC_URL + "/data/players.json")
             .then(response => response.json())
             .then(arrayOfPlayers => this.setState({ players: arrayOfPlayers }));
+            console.log(this.state.players)
     }
 
     render() {
-
         const tournamentId = this.props.tournamentId
-        const games = this.state.tournaments.filter(
+        
+        const tournament = this.state.tournaments.filter( 
             tournament => tournament.id === tournamentId
-        )
-        console.log(this.state.tournaments)
-        console.log(this.state.players)
+        )[0]
+        console.log(tournament) 
+        // console.log(tournament.games)
+
+        const games = tournament
         console.log(games)
+
+        // const tournamentStatus = tournament.status
+        // console.log(tournamentStatus)
         return (
             <div className="TournamentView-container">
                 {/* <TournamentInfo/> */}
