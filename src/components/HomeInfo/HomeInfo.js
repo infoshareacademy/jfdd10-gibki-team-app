5530a4d2789881fa9e1475b4c8bf34a2ea07d0db
ankaziele <ankaziele@gmail.com>
2018-10-16 16:33:11 +0200

    initial commit for homeinfo

---
+++ src/components/HomeInfo/HomeInfo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class HomeInfo extends Component {
+    state = {  }
+    render() { 
+        return ( 
+
+        <header>
+            <h1>Enter the Tournament and Have Some Fun!</h1>
+        </header>
+         );
+    }
+}
+ 
+export default HomeInfo;
