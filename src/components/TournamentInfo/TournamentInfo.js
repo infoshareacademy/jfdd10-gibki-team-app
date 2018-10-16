21e00def13fc81a627a6ac1ba66dbe9ea2f4d716
ankaziele <ankaziele@gmail.com>
2018-10-16 16:08:40 +0200

    add tournament info

--- src/components/TournamentInfo/TournamentInfo.js
+++ src/components/TournamentInfo/TournamentInfo.js
@@ -7,14 +7,36 @@ class TournamentInfo extends Component {
     name: PropTypes.string.isRequired,
     date: PropTypes.string.isRequired,
     address: PropTypes.string.isRequired,
-    status: PropTypes.string.isRequired,
+    status: PropTypes.string.isRequired
   };
 
   render() {
     return (
-      <header className="tournamentHeader">
-      <h1>{this.props.name}</h1>
-      
+      <header className="tournamentInfo-Header">
+        <h1>{this.props.name}</h1>
+        <div className="tournamentInfo-container">
+          <img
+            className="tournamentInfo-image"
+            src={this.props.image}
+            alt="Tournament view"
+          />
+          <div className="tournamentInfo-info">
+            <div className="tournamentInfo-DataNames">
+              <h2 className="tournamentInfo-h2">Date:</h2>
+              <h2 className="tournamentInfo-h2">City:</h2>
+              <h2 className="tournamentInfo-h2">Places available:</h2>
+              <h2 className="tournamentInfo-h2">Status:</h2>
+            </div>
+
+            <div className="tournamentInfo-data">
+              <h2 className="tournamentInfo-h2">{this.props.date}</h2>
+              <h2 className="tournamentInfo-h2">{this.props.city}</h2>
+              <h2 className="tournamentInfo-h2">{this.props.points}</h2>
+              <h2 className="tournamentInfo-h2">{this.props.places}</h2>
+              <h2 className="tournamentInfo-h2">{this.props.status}</h2>
+            </div>
+          </div>
+        </div>
       </header>
     );
   }
