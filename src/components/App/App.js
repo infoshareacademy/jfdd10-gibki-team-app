3bdf8f5ffb7ef85d00409b948cae9294a72f7488
Tomasz Milaszewski <tomasz.milaszewski@gmail.com>
2018-10-16 15:56:03 +0200

    104 solves score list view for tournament in progress

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <TournamentView tournamentId={2}/>
+        <TournamentView tournamentId={5}/>
       </div>
     );
   }
