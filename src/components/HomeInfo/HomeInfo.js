4f1859590d8b56bfa94b5f7e27fa3f9491507998
Tomek_M <tomasz.milaszewski@gmail.com>
2018-10-17 20:18:31 +0200

    adds HomeInfo to HomeView

--- src/components/HomeInfo/HomeInfo.js
+++ src/components/HomeInfo/HomeInfo.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 import "./HomeInfo.css";
 class HomeInfo extends Component {
     static propTypes = {
--- src/components/HomeView/HomeView.js
+++ src/components/HomeView/HomeView.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import HomeInfo from "../HomeInfo/HomeInfo";
 import TournamentList from "../TournamentList/TournamentList";
 import "./HomeView.css";
 
@@ -6,7 +7,7 @@ class HomeView extends Component {
   render() {
     return (
       <div className="HomeView-container">
-        {/* <HomeInfo/> */}
+        <HomeInfo/>
         <TournamentList />
       </div>
     );
