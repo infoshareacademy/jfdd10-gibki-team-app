96de8677b57669ceeefbb5e49ba1254d2439b5f4
ankaziele <ankaziele@gmail.com>
2018-10-18 16:00:58 +0200

    add players view to app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import HomeView from "../HomeView/HomeView";
 import TournamentView from "../TournamentView/TournamentView";
 import PlayerView from "../PlayerView/PlayerView";
+import PlayersView from "../PlayersView/PlayersView"
 import "./App.css";
 
 class App extends Component {
@@ -12,7 +13,8 @@ class App extends Component {
         <div className="App">
           <Route exact path="/" component={HomeView} />
           <Route path="/TournamentView/:tournamentId" component={TournamentView} />
-          <Route path="/PlayerView" component={PlayerView} />
+          <Route path="/PlayerView/:playerId" component={PlayerView} />
+          <Route path="/PlayersView" component={PlayersView} />
         </div>
       </Router>
     );
