7513a5c5e7192a2d5291c1fbc554b6da7a9f0851
ankaziele <ankaziele@gmail.com>
2018-10-18 16:57:12 +0200

    change of home info view

--- src/components/HomeInfo/HomeInfo.js
+++ src/components/HomeInfo/HomeInfo.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-// import PropTypes from "prop-types";
 import "./HomeInfo.css";
 class HomeInfo extends Component {
     static propTypes = {
@@ -11,6 +10,10 @@ class HomeInfo extends Component {
 
         <header className="HomeInfo-Header">
             <h1 className="HomeInfo-H1">Enter the Tournament and Have Some Fun!</h1>
+            <div class="image-container">
+            <img class="image-hand-left" src="./left-hand.png" alt="Left Hand" />
+            <img class="image-hand-right" src="./right-hand.png" alt="Right Hand" />
+            </div>
             <button><Link to={"/PlayersView"}>Players</Link></button>
         </header>
          );
