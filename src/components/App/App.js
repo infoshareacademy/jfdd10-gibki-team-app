ccea9208e0118b2a1cbcb4b2424703a36431eeca
kruzyk <kruzyk@gmail.com>
2018-10-23 15:05:00 +0200

    WIP MUI theme

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -21,7 +21,7 @@ const THEME = createMuiTheme({
 class App extends Component {
   render() {
     return (
-      <MuiThemeProvider theme={THEME}>
+      <MuiThemeProvider theme={THEME} key={Date.now()}>
         <Router>
           <div className="App">
             <Route exact path="/" component={HomeView} />
