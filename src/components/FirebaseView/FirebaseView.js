1056401010227d20af1aa8a6a4139540b4756d2e
ankaziele <ankaziele@gmail.com>
2018-10-25 15:38:07 +0200

    add sign in form, sends data to Firebase

---
+++ src/components/FirebaseView/FirebaseView.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class FirebaseView extends Component {
+    state = { 
+        email: "",
+        password: ""
+
+     }
+
+    handleChange = event => (
+        this.setState( {[event.target.name]: event.target.value})
+    )
+
+
+handleSubmit = event => {
+    event.preventDefault();
+    fetch("https://first-project-fe601.firebaseio.com/players.json", {
+      method: "POST",
+      body: JSON.stringify({
+        email: this.state.email,
+        password: this.state.password
+      }),
+     
+    });
+  };
+    
+    render() { 
+        return ( 
+            <div>
+                <form onSubmit={ this.handleSubmit}>
+                    <input placeholder="email" name="email" value={this.state.email} onChange={this.handleChange}></input>
+                    <input placeholder="password" name="password" value={this.state.password} onChange={this.handleChange}></input>
+                    <button>Register</button>
+                </form>
+            </div>
+         );
+    }
+}
+ 
+export default FirebaseView;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '.setupFirebase';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
