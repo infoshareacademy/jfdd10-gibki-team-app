a57d57edeafa8d7ef77ac7ee648ca29191ddc6e8
Emil Pierzchalski <pierzchalski.emil@gmail.com>
2018-10-26 16:53:46 +0200

    WIP SignIn  styling

--- src/components/HomeView/HomeView.js
+++ src/components/HomeView/HomeView.js
@@ -3,11 +3,13 @@ import HomeInfo from "../HomeInfo/HomeInfo";
 import TournamentList from "../TournamentList/TournamentList";
 import "./HomeView.css";
 import AuthComponent from "../AuthComponent/AuthComponent";
+import FormDialog from "../AuthComponent/example"
 class HomeView extends Component {
   render() {
     return (
       <div className="HomeView-container">
         <HomeInfo/>
+        <FormDialog/>
         <AuthComponent/>
         <TournamentList />
       </div>
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react'
 import firebase from 'firebase'
+// import React from 'react';
+// import Button from '@material-ui/core/Button';
+// import TextField from '@material-ui/core/TextField';
+// import Dialog from '@material-ui/core/Dialog';
+// import DialogActions from '@material-ui/core/DialogActions';
+// import DialogContent from '@material-ui/core/DialogContent';
+// import DialogContentText from '@material-ui/core/DialogContentText';
+// import DialogTitle from '@material-ui/core/DialogTitle';
 
 class SignIn extends Component {
     state = { 
         email: "",
         password: "",
-        error: null
+        error: null,
+        open: false
 
      }
 
     handleChange = event => (
         this.setState( {[event.target.name]: event.target.value})
     )
+    
+    handleClickOpen = () => {
+        this.setState({ open: true });
+      };
 
 
     handleSubmit = event => {
@@ -24,8 +37,8 @@ class SignIn extends Component {
         ).catch(
           error => this.setState({ error })
         )
-        this.setState({email: "",
-    password: "" })
+        this.setState({ open: false })
+        this.setState({email: "", password: "" })
       }
 
   componentDidMount(){
@@ -48,6 +61,38 @@ class SignIn extends Component {
                     <button>Sign In</button>
                 </form>
             </div>
+        //     <div>
+        //     <Button onClick={this.handleClickOpen}>Sign In</Button>
+        //     <Dialog
+        //       open={this.state.open}
+        //       onClose={this.handleClose}
+        //       aria-labelledby="form-dialog-title"
+        //     >
+        //       <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+        //       <DialogContent>
+        //         <DialogContentText>
+        //           To subscribe to this website, please enter your email address here. We will send
+        //           updates occasionally.
+        //         </DialogContentText>
+        //         <TextField
+        //           autoFocus
+        //           margin="dense"
+        //           id="name"
+        //           label="Email Address"
+        //           type="email"
+        //           fullWidth
+        //         />
+        //       </DialogContent>
+        //       <DialogActions>
+        //         <Button onClick={this.handleClose} color="primary">
+        //           Cancel
+        //         </Button>
+        //         <Button onClick={this.handleClose} color="primary">
+        //           Subscribe
+        //         </Button>
+        //       </DialogActions>
+        //     </Dialog>
+        //   </div>
          );
     }
 }
