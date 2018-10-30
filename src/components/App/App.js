6a7686a2ecdc70f13668814c1f62ac3a2158fb9b
kruzyk <kruzyk@gmail.com>
2018-10-30 15:37:57 +0100

    deletes double const THEME

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -28,26 +28,6 @@ const THEME = createMuiTheme({
     }
   }
 });
-
-const theme = createMuiTheme({
-  overrides: {
-    // Name of the component ⚛️ / style sheet
-    MuiButton: {
-      // Name of the rule
-      root: {
-        // Some CSS
-        background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-        borderRadius: 3,
-        border: 0,
-        color: "white",
-        height: 48,
-        padding: "0 30px",
-        boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)"
-      }
-    }
-  }
-});
-
 class App extends Component {
   render() {
     return (
