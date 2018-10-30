f04f224aa2ac93bfb59b642e9ab360bb911c0416
Tomasz Milaszewski <tomasz.milaszewski@gmail.com>
2018-10-30 15:26:15 +0100

    changes PlayerList styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -15,7 +15,37 @@ const THEME = createMuiTheme({
    "fontWeightLight": 300,
    "fontWeightRegular": 400,
    "fontWeightMedium": 500
+  },
+  overrides: {
+    MuiButton: {
+      root: {
+        backgroundColor: '#6573c3',
+        '&:hover': {
+          backgroundColor: '#2c387e'
+      }
+      },
+    },
+
   }
+});
+
+const theme = createMuiTheme({
+  overrides: {
+    // Name of the component ⚛️ / style sheet
+    MuiButton: {
+      // Name of the rule
+      root: {
+        // Some CSS
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        borderRadius: 3,
+        border: 0,
+        color: 'white',
+        height: 48,
+        padding: '0 30px',
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      },
+    },
+  },
 });
 
 class App extends Component {
