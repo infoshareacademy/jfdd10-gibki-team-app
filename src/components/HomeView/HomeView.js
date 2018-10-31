dfb190b61d29b4d6f35a51d5cfecb6b09e3125a5
Emil Pierzchalski <pierzchalski.emil@gmail.com>
2018-10-31 15:21:22 +0100

    changed position of sign in/up buttons

--- src/components/HomeView/HomeView.js
+++ src/components/HomeView/HomeView.js
@@ -2,14 +2,13 @@ import React, { Component } from "react";
 import HomeInfo from "../HomeInfo/HomeInfo";
 import TournamentList from "../TournamentList/TournamentList";
 import "./HomeView.css";
-import AuthComponent from "../AuthComponent/AuthComponent";
+
 
 class HomeView extends Component {
   render() {
     return (
       <div className="HomeView-container">
         <HomeInfo/>
-        <AuthComponent/>
         <TournamentList />
       </div>
     );
