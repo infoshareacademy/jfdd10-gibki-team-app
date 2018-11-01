afa904e572006a5591f5949597a4077944d4e90d
Tomek_M <tomasz.milaszewski@gmail.com>
2018-11-01 18:41:11 +0100

    hides Join button when no places are left

--- src/components/TournamentView/TournamentView.js
+++ src/components/TournamentView/TournamentView.js
@@ -71,10 +71,12 @@ class TournamentView extends Component {
               tournamentPlayers={this.state.tournamentPlayers}
               playerListHeader={"Players taking part"}
             />{" "}
+            {this.state.tournamentPlayers.length < 8 ? (
             <div className="JoinTournament-container">
               <h1>Join Tournament</h1>
               <JoinTournamentForm tournamentId={this.props.location.state.tournamentId} tournamentPlayers={this.state.tournamentPlayers}/>{" "}
             </div>
+            ) : (<div></div>)}
           </div>
         ) : (
           <ScoreList games={this.state.games} players={this.state.players} />
